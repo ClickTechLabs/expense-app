67f81352d8a87bed049869f0874d33c14e8474c8
ClickTeck <support@clickteck.com>
2017-06-06 14:30:33 +0500

    added SQ Lite Plugin in add category page

--- src/pages/add-category/add-category.ts
+++ src/pages/add-category/add-category.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
-/**
- * Generated class for the AddCategory page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-add-category',
@@ -24,7 +18,9 @@ public database: SQLite;
   constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, private sqlite: SQLite) {
   	this.platform.ready().then(() => {
 	  		this.sqlite.create({ name: 'data.db', location: 'default' }).then((db: SQLiteObject) => {
+
 					    db.executeSql('CREATE TABLE IF NOT EXISTS categories(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32), color VARCHAR(32))', {}).then((data) => {
+                 console.log('Hello');
 					    	console.log("TABLE CREATED: ", data);
 					    }, (error) => {
 						    console.error("Unable to execute sql", error);
@@ -55,17 +51,18 @@ public database: SQLite;
   }*/
 
   addCategory(){
-  	console.log(this.colors);
   	if (this.category_name == "") {
       //this.cmn.alert('Warning', 'Enter pool name');
     }
     else {
-    	//error here
+      console.log(this.category_name, this.colors);
             this.sqlite.create({name: "data.db", location: "default"}).then((db: SQLiteObject) => {
-                db.executeSql('INSERT INTO categories (name, color) VALUES (this.name, this.colors)', {}).then((data) => {
+              console.log('Zille');
+                db.executeSql("INSERT INTO categories (name, color) VALUES (" + this.category_name + ',' + this.colors + ")", []).then((data) => {
+                  console.log('data');
                 	console.log("INSERTED: " + JSON.stringify(data));
                 }, (error) => {
-            		console.log("ERROR: " + JSON.stringify(error.err));
+            		console.log("ERROR: " + JSON.stringify(error));
         		});
                 });
 
@@ -77,17 +74,31 @@ public database: SQLite;
   }
 
   refresh(){
-  	  this.platform.ready().then(() => {
-  	        this.database = new SQLite();
-            this.database.create({name: "data.db", location: "default"}).then((db: SQLiteObject) => {
-                db.executeSql("SELECT * FROM categories", {}).then((data) => {
-                	           if(data.rows.length > 0) {
-                					for(var i = 0; i < data.rows.length; i++) {
-                    				this.categoryList.push({ category_name: data.rows.item(i).name, color: data.rows.item(i).color});
-			                }
-			            }
+      this.platform.ready().then(() => {
+            this.sqlite.create({name: "data.db", location: "default"}).then((db: SQLiteObject) => {
+                db.executeSql("SELECT * FROM categories", []).then((data) => {
+                  console.log(data);
+                             if(data.rows.length > 0) {
+                          for(var i = 0; i < data.rows.length; i++) {
+                            this.categoryList.push({ category_name: data.rows.item(i).name, color: data.rows.item(i).color});
+                      }
+                  }
                 }, (error) => {
-            		console.log("ERROR: " + JSON.stringify(error.err));
+                console.log("ERROR: " + JSON.stringify(error));
+            });
+            }, (error) => {
+                console.log("ERROR: ", error);
+            });
+        });
+  }
+
+  delete(){
+  	  this.platform.ready().then(() => {
+            this.sqlite.create({name: "data.db", location: "default"}).then((db: SQLiteObject) => {
+                db.executeSql("DELETE FROM categories", []).then((data) => {
+                  console.log(data);
+                }, (error) => {
+            		console.log("ERROR: " + JSON.stringify(error));
         		});
             }, (error) => {
                 console.log("ERROR: ", error);
